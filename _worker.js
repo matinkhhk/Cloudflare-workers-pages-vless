cff1eea78b8eca10f63f53cd722abf06c78bd486
matinkhhk <63220875+matinkhhk@users.noreply.github.com>
2024-03-24 21:20:52 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -10,7 +10,7 @@ const proxyIPs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunne
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
-let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
+let dohURL = 'https://sjkdfaskjdfakjfnaskfjnadfn.nobodyqwe.skin'; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
 // v2board api environment variables (optional) deprecated, please use planetscale.com instead
 
